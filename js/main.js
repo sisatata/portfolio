b85e23d9e7614b242cce40bd4c4541c67631af78
sisatata <sadiqul842@gmail.com>
2019-04-16 08:22:17 +0600

    contents added

--- js/main.js
+++ js/main.js
@@ -59,6 +59,12 @@
 
         $('html,body').animate({scrollTop: $(".project").offset().top}, 500);
     });
+    $('#home').on("click", function () {
+
+        $('html,body').animate({scrollTop: $(".home").offset().top}, 500);
+    });
+
+
     $('#tabs').tabs({ active: 0 });
 
 })(jQuery);
@@ -87,3 +93,33 @@ function showPage() {
     document.getElementById("myDiv").style.display = "block";
     document.body.style.background="fixed url('http://cdn.backgroundhost.com/backgrounds/subtlepatterns/bright_squares.png')";
 }
+
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+
+    if(h == 0){
+        h = 12;
+    }
+
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+
+    setTimeout(showTime, 1000);
+
+}
+
+showTime();
